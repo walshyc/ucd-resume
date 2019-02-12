0c9f2ee32e4b04e4aa74b13c56bae1368ee6c051
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-12 10:55:25 +0000

    fixed api throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -67,6 +67,9 @@ $("#gh-repo-data").html("");
         }, function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            }else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
@@ -76,3 +79,4 @@ $("#gh-repo-data").html("");
 }
 
 $(document).ready(fetchGitHubInformation);
+
