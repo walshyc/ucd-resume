f05098196845ed6d9bc7f8efcf5f52b985bed5ee
Conor Walsh <conorwalsh0703@gmail.com>
2019-02-12 10:47:09 +0000

    Fixed some bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -39,6 +39,10 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+
+$("#gh-user-data").html("");
+$("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please Enter a GitHub username</h2>`);
@@ -70,3 +74,5 @@ function fetchGitHubInformation(event) {
         })
 
 }
+
+$(document).ready(fetchGitHubInformation);
